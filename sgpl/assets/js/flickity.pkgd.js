03d3478ee37f7f8c9c3fb256c18039e389a3a563
Lewis Darnton PC <lewisdarnton@googlemail.com>
2019-02-16 13:14:55 +0000

    1

--- sgpl/assets/js/flickity.pkgd.js
+++ sgpl/assets/js/flickity.pkgd.js
@@ -4581,19 +4581,3 @@ proto.imagesLoaded = function() {
 return Flickity;
 
 }));
-
-var $carousel = $('.carousel').flickity({
-  initialIndex: 1
-});
-var $logger = $('.logger');
-
-$carousel.on( 'staticClick.flickity', function( event, pointer, cellElement, cellIndex ) {
-  // dismiss if cell was not clicked
-  if ( !cellElement ) {
-    return;
-  }
-  // change cell background with .is-clicked
-  $carousel.find('.is-clicked').removeClass('is-clicked');
-  $( cellElement ).addClass('is-clicked');
-  $logger.text( 'Cell ' + ( cellIndex + 1 )  + ' clicked' );
-});
